8cf8c5840e57b7b0e0276419861439a87302a6dd
BANOTH MOHAN <banothmohan1206@gmail.com>
2019-02-09 13:16:42 +0530

    FAKE-API-EMP-DATA

---
+++ script.js
@@ -0,0 +1,18 @@
+const xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    const res = JSON.parse(xhttp.responseText);
+    let output = '';
+    for( i = 0; i < 20; i++ ) {
+        output += `
+                    <tr>
+                        <td>${res[i].id}</td>
+                        <td>${res[i].employee_name}</td>
+                        <td>${res[i].employee_age}</td>
+                        <td>${res[i].employee_salary}</td>
+                    </tr>
+                    </table`;
+    }
+    document.getElementById('uls').innerHTML = output;
+}
+xhttp.open("GET", "http://dummy.restapiexample.com/api/v1/employees", true)
+xhttp.send();
